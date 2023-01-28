17dc62aa99f308b69abf317d65c5893f0adea2c4
angelsalazar <angelsalazar1992@hotmail.com>
2023-01-28 17:46:11 -0600

    adding ThreadSum

---
+++ Main.java
@@ -0,0 +1,80 @@
+public class Main {
+
+    /**
+     * createListOfNumbers 
+     * creates an array of the given size
+     * which contains integers that are bound in the given range
+     * @param size desire array size
+     * @param min specifies the minimum value of the randomly generated integer
+     * @param max specifies the maximum value of the randomly generated integer
+     * @return int[]
+     */
+    public static int[] createListOfNumbers(int size, int min, int max) {
+        // Initializing array of the given size
+        int[] output = new int[size];
+
+        // populating array with random numbers
+        // that are bound to the lower and upper bound
+        for (Integer i = 0; i < size; i++) {
+            output[i] = (int)(
+                Math.floor(
+                    Math.random() * (max - min + 1) + min
+                )
+            );
+        }
+
+        // return array
+        return output;
+    }
+
+    /**
+     * parallelSum
+     * Sums to arrays. computing the output array is based on threads where each thread will process 
+     * a slice of the given arrays (this specify by the chunkSize)
+     * 
+     * 
+     * @param output holds the sum of the array a + array b
+     * @param a array to sum
+     * @param b array to sum
+     * @param chunkSize specifies the slices of the array that should be processed by a threat
+     */
+    public static void parallelSum(int[] output, int[] a, int[] b, int chunkSize) {
+        // computing the array length
+        // it is assumed that the arrays a and b have the same size
+        int arrayLength = a.length;
+
+        // computing the number of threads to comput the output array
+        int chunks = arrayLength / chunkSize;
+        
+        for (int i = 0; i < chunks; i++) {
+            // computing the slice of the array 
+            // will be processed by a thread
+            int start = i * chunkSize;
+            int end = (i * chunkSize) + chunkSize;
+
+            System.out.println("Start: " + start + " end: " + end);
+
+            // initializng ThreadSum
+            // specifying the slice of the array will processed
+            ThreadSum worker = new ThreadSum(output, a, b, start, end);
+            // running thread
+            worker.start();
+        }
+    }
+    
+
+    public static void main(String[] args) throws InterruptedException {
+        // specifying the length of the arrays
+        int size = 10;
+        // creating an array of intenger whose values will be between 1 and 10
+        int[] a = createListOfNumbers(size, 1, 10);
+        // creating an array of intenger whose values will be between 20 and 30
+        int[] b = createListOfNumbers(size, 20, 30);
+        // initializing output array
+        int[] output = new int[size];
+        
+        // executing sum in parallale
+        parallelSum(output, a, b, 5);
+    }
+
+}
---
+++ ThreadSum.java
@@ -0,0 +1,89 @@
+/**
+ * ThreadSum 
+ * sums an slice of the given arrays
+ */
+public class ThreadSum extends Thread {
+    // holds the sum of the array a and b
+    private int[] output;
+
+    // holds the array the will be added
+    private int[] a;
+
+    // holds the array the will be added
+    private int[] b;
+
+    // specifies where the slice starts
+    private int start;
+
+    // specifies where the slice ends
+    private int end;
+
+    /**
+     * initializes ThreadSum object
+     * @param output
+     * @param a
+     * @param b
+     * @param start
+     * @param end
+     */
+    public ThreadSum(int[] output, int[] a, int[] b, int start, int end) {
+        this.output = output;
+        this.a = a;
+        this.b = b;
+        this.start = start;
+        this.end = end;
+    }
+
+    /**
+     * serialize
+     * serializes the given array of intengers into a string by joining them using \t as delimitter
+     * @param arr
+     * @return String
+     */
+    public static String serialize(int[] arr) {
+        // holds the serielized array
+        StringBuilder builder = new StringBuilder("");
+
+        for (Integer i = 0; i < arr.length; i++) {
+            // adding entry to the string
+            builder.append(String.valueOf(arr[i]));
+            // if i is not the last item
+            // then add delimitter
+            if (i != arr.length - 1) {
+                builder.append(" \t ");
+            }
+        }
+
+        // return serielized array
+        return builder.toString();
+    }
+    
+    /**
+     * run
+     * specifies that work that needs to be performed by ThreadSum
+     * sums a slice of array a and array b and store it in output
+     */
+    @Override
+    public void run() {
+        // process the specifed slice
+        for (int i = this.start; i < this.end; i++) {
+            // store the sum of item a and item b
+            this.output[i] = this.a[i] + this.b[i];
+            
+            // adding Thread.sleep to prevent overlapping when printing
+            // NOTE: This can be ommitted
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException ex) {
+                ex.printStackTrace();
+            }
+            
+            // outputing how output gets filled out
+            System.out.println("Thread processing slice from index " + this.start + " to " + this.end);
+            System.out.println("A " + serialize(this.a));
+            System.out.println("B " + serialize(this.b));
+            System.out.println("R " + serialize(this.output));
+            System.out.println("Thread processing slice from index " + this.start + " to " + this.end + " END \n");
+        }
+    }
+}
